10307d18c928e19cd0c7f983d9d3bf492dc2f832
ftorrelles <torrellesf93@gmai.com>
2023-02-09 15:37:52 -0400

    change

--- src/componets/CardCocktails.jsx
+++ src/componets/CardCocktails.jsx
@@ -4,13 +4,6 @@ const CardCocktails = ({ dataCocktails }) => {
     const { strDrinkThumb, strDrink, strInstructions } = dataCocktails;
     return (
         <div className="card">
-            {/* <div className="card_img">
-                <img className="img_cocktail" src={strDrinkThumb} alt="" />
-            </div>
-            <div className="card_info">
-                <h2>{strDrink}</h2>
-                <p>{strInstructions}</p>
-            </div> */}
             <Card className="card_cocktail" style={{ border: "none" }}>
                 <Card.Img variant="top" src={strDrinkThumb} />
                 <Card.Body style={{ padding: "0.3rem" }}>
